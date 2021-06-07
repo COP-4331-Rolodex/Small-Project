70d8da1ac47516eab4038727d6e87c9555edc0f6
Timothy Ziemathis <timothyziemathis@gmail.com>
2021-06-07 17:22:22 -0400

    Add files via upload
    API to list all the contacts when the page is loaded.

---
+++ List.php
@@ -0,0 +1,83 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
+    error_reporting(E_ALL); // reports all errors
+    ini_set("display_errors", "1"); // shows all errors
+    ini_set("log_errors", 1);
+    ini_set("error_log", "/tmp/php-error.log");
+
+        $inData = getRequestInfo();
+
+        $searchResults = "";
+        $searchCount = 0;
+
+    $servername = "localhost";
+    $username = "dev";
+    $password = "knights";
+    $database = "Contacts";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
+        if ($conn->connect_error)
+        {
+                returnWithError( $conn->connect_error );
+        }
+        else
+        {
+        //$id = 1;
+
+            $stmt = $conn->prepare("SELECT * from contacts where owner_id=?");
+
+            $stmt->bind_param("i", $inData["id"]);
+            //$stmt->bind_param("i", $id);
+
+            $stmt->execute();
+
+                $result = $stmt->get_result();
+
+                $searchResults = array();
+
+                while($row = $result->fetch_assoc())
+                {
+                        $searchResults[$searchCount] = $row["first_name"]. ' ' . $row["last_name"]. ' ' . $row["email"] . ' '. $row["phone"] . ' ' . $row["id"];
+                        $searchCount++;
+                }
+
+                if( $searchCount == 0 )
+                {
+                        returnWithError( "No Records Found" );
+                }
+                else
+                {
+                        sendResultInfoAsJson( $searchResults );
+                }
+
+                $stmt->close();
+                $conn->close();
+        }
+
+        function getRequestInfo()
+        {
+                return json_decode(file_get_contents('php://input'), true);
+        }
+
+        function sendResultInfoAsJson( $obj )
+        {
+                header('Content-type: application/json');
+                $length = count($obj);
+                for ($i = 0; $i < $length; $i++) {
+                echo $obj[$i];
+                echo "\r\n";
+                }
+        }
+
+        function returnWithError( $err )
+        {
+                $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+                sendResultInfoAsJson( $retValue );
+        }
+
+?>
